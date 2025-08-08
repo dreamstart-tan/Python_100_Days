a8c74605ec84c97a3ccd349ca89cfedf8d09ac48
dreamstart-tan <txpmail@163.com>
2025-08-08 14:41:49 +0800

    msg

---
+++ Day1-20/res/day06/test01.py
@@ -0,0 +1,19 @@
+import random 
+answer = random.randrange(1,101)
+counter = 0 
+while True:
+    counter += 1
+    num = int(input('请输入:'))
+    if num < answer:
+        print('大一点:')
+    elif num > answer:
+        print('小一点')
+    else:
+        print('猜对了.')
+print(f'你一共猜对了{counter}次.')
+
+
+
+
+
+
