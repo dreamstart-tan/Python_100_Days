7265a1dd86d5573da0d6cc27161a31d5991c3192
dreamstart-tan <txpmail@163.com>
2025-08-09 17:43:36 +0800

    msg

--- Day1-20/res/day03/day3_tets01.py
+++ Day1-20/res/day03/day3_tets01.py
@@ -1,7 +1,6 @@
-year = int(input('请输入年份:'))
-is_leap = year % 4 ==0 and year % 100 != 0 or year % 400 == 0 
-# print(f'{is_leap = }')
-print(is_leap)
-
-
+a = 10 
+b = 3
+a += b  # a = a+ b
+a *= a + 2 # a = a *(a+2)
+print(a) 
 
